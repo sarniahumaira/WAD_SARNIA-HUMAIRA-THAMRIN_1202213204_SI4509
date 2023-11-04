f6ad2515c9909bf7b6ebd70a2d72f19b344c3bf4
sarniahumaira <sarniahumaira18@gmail.com>
2023-11-04 13:03:54 +0700

    TUGAS 3

---
+++ tambah.php
@@ -0,0 +1,57 @@
+<?php
+
+require "functions.php";
+//cek apakah tombol submit sudah ditekan apa belum
+if (isset($_POST["submit"])){
+
+    //cek apakah data berhasil ditambahkan
+    if (tambah($_POST) > 0){
+        echo "data berhasil ditambahkan!";
+    } else {
+        echo "data gagal ditambahkan";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tmabha data mahasiswa</title>
+</head>
+<body>
+    <h1>Tambah data mahasiswa</h1>
+
+    <form action="" method="POST">
+        <ul>
+            <li>
+                <label for="nama">Nama : </label>
+                <input type="text" name="nama" id="nama">
+            </li>
+            <li>
+                <label for="nrp">NRP : </label>
+                <input type="text" name="nrp" id="nrp">
+            </li>
+            <li>
+                <label for="email">Email : </label>
+                <input type="text" name="email" id="email">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan : </label>
+                <input type="text" name="jurusan" id="jurusan">
+            </li>
+            <li>
+                <label for="gambar">Gambar : </label>
+                <input type="text" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Tambah Data</button>
+            </li>
+        </ul>
+    </form>
+</body>
+</html>
+
+
+
